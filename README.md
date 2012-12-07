785c9838be024c763d6cc90f3812fb53b5ec9a64
Adrian Espinosa <aespinosamoreno@gmail.com>
2012-12-07 20:22:52 +0100

    Improved code. Now using praw and pil. Added support for Picsarus

--- README.md
+++ README.md
@@ -3,29 +3,43 @@ download-reddit-saved-images
 
 This script checks your saved links and searchs for images to automagically download them into a folder.
 
-INSTRUCTIONS
-=============
-1. Enter your reddit's username and password
-2. Set a directory where images will be stored
-3. To find you 'saved_url', go to reddit.com -> preferences -> RSS Feeds and choose JSON saved links (it is important to choose JSON and not RSS)
+Instructions
+---
+1. Clone the repository
+2. Enter your reddit's username and password
+3. Set a directory where images will be stored
 4. Run the script and see how your saved images are downloaded :)
 
-WARNING:
-Items downloaded will be unsaved. If you want to prevent this, remove or comment the "unsaving block"
+**WARNING:**
+Items downloaded **will be unsaved**. If you want to prevent this, remove all occurrences of unsave()
+
+Changelog
+---
+* 0.2.1.1
+* * Improved code thanks to */u/shaggorama*
+* * Now using PRAW and PIL
+* * Added support for Picsarus
 
 
-SUPPORT
-=========
+* 0.1.1.1
+* * Initial release
+
+
+Support
+---
 1. Direct links to images
 2. Imgur links
 3. Imgur albums
 4. Flickr images
 5. Tumblr  (not fully tested)
+6. Picsarus
 
-If you want to collaborate, to improve performance or to add support to another websites, please submit pull requests.
+**If you want to collaborate, to improve performance or to add support to another websites, please submit pull requests.**
 
 
-REQUIREMENTS
-==
+Requirements
+---
 1. BeautifulSoup 4
 2. Requests
+3. Praw
+4. PIL
--- script.py
+++ script.py
@@ -1,67 +1,175 @@
 import requests
-import json
 import os
 from bs4 import BeautifulSoup as Soup
 from zipfile import ZipFile
-import time
+from PIL import Image
+import praw
+from StringIO import StringIO
 
 
 __author__ = 'Adrian Espinosa'
+__version__ = '0.2.1.1'
+__contributor__ = '/u/shaggorama'
+
+image_formats = ['bmp', 'dib', 'eps', 'ps', 'gif', 'im', 'jpg', 'jpe', 'jpeg',
+                  'pcd', 'pcx', 'png', 'pbm', 'pgm', 'ppm', 'psd', 'tif', 'tiff',
+                  'xbm', 'xpm', 'rgb', 'rast', 'svg']
 
 
-def detect_ext(content):
+def is_image_link(submission):
     """
-    Detect image extension
+    Takes a praw.Submission object and returns a boolean
+    describing whether or not submission links to an
+    image.
     """
-    if content.startswith('\xff'):
-        return ".jpg"
-    elif content.startswith("GIF89a"):
-        return ".gif"
-    elif content.startswith("\x89PNG"):
-        return ".png"
-    else:
-        return ".jpg"
-
-
-def check_size(filename):
-    """
-    Calculate file size.
-    To check if file is corrupt.
-    """
-    stats = os.stat(filename)
-    filesize = stats.st_size
-    # 2 KB
-    if filesize > 2048:
+    if submission.url.split('.')[-1] in image_formats:
         return True
     else:
         return False
 
+
 # user data
 username = ''
 password = ''
-save_dir = '/path/to/store/imgs/'
+save_dir = ''
 album_path = os.path.join(save_dir, 'albums')
-saved_url = ''
-login_payload = {'user': username, 'passwd': password, 'api_type': 'json'}
-headers = {'user-agent': 'aesptux\'s saved images downloader'}
-login_url = 'http://www.reddit.com/api/login'
-unsave_url = 'http://www.reddit.com/api/unsave'
+# to notify errors
+errors = []
+
+
+class Downloader(object):
+    global errors
+
+    def __init__(self, submission):
+        self.submission = submission
+        self.path = os.path.join(save_dir, str(submission.created) + submission.title[0:20])
+        self.album_path = os.path.join(self.path, 'albums')
+        print "Downloading --> %s" % (submission.title)
+
+    def direct_link(self):
+        try:
+            photo = requests.get(self.submission.url)
+            image = Image.open(StringIO(photo.content))
+            image.verify()
+            Image.open(StringIO(photo.content)).save(self.path + "." + image.format.lower())
+            self.submission.unsave()
+        except Exception, e:
+            errors.append(self.submission.title)
+            print e
+
+    def imgur_album(self):
+        download_url = 'http://s.imgur.com/a/%s/zip' % (os.path.split(self.submission.url)[1])
+        filezip = requests.get(download_url)
+        path = os.path.join(album_path, self.submission.title[0:50])
+        # extract zip
+        if not os.path.exists(path):
+            os.mkdir(path)
+        try:
+            i = open(path + '.zip', 'w')
+            i.write(filezip.content)
+            i.close()
+            zipfile = ZipFile(path + '.zip')
+            zipfile.extractall(path)
+            self.submission.unsave()
+        except Exception, e:  # big album
+            os.remove(path + '.zip')
+            print e
+            print "Album is too big, downloading images..."
+            # this is the best layout
+            idimage = os.path.split(self.submission.url)[1]
+            if '#' in idimage:
+                idimage = idimage[0:idimage.index("#")]
+            url = "http://imgur.com/a/%s/layout/blog" % (idimage)
+            web = requests.get(url)
+            soup = Soup(web.content)
+            container = soup.find("div", {"id": "image-container"})
+            imgs = container.findAll("a", {"class": "zoom"})
+            counter = 0
+            for img in imgs:
+                print counter
+                p = img.attrs['href']
+                try:
+                    photo = requests.get(p)
+                    image = Image.open(StringIO(photo.content))
+                    image.verify()
+                    Image.open(StringIO(photo.content)).save(self.path + "." + image.format.lower())
+                    self.submission.unsave()
+                except Exception, e:
+                    errors.append(self.submission.title)
+                    print e
+
+    def imgur_link(self):
+        # just a hack. i dont know if this will be a .jpg, but in order to download an
+        # image data, I have to write an extension
+        new_url = "http://i.imgur.com/%s.jpg" % (os.path.split(self.submission.url)[1])
+        try:
+            photo = requests.get(new_url)
+            image = Image.open(StringIO(photo.content))
+            image.verify()
+            Image.open(StringIO(photo.content)).save(self.path + "." + image.format.lower())
+            self.submission.unsave()
+        except Exception, e:
+            errors.append(self.submission.title)
+            print e
+
+    def tumblr_link(self):
+        photo = requests.get(self.submission.url)
+        soup = Soup(photo.content)
+        #div = soup.find("div", {'class': 'post'})
+        #if not div:
+        #    div = soup.find("li", {'class': 'post'})
+        links = soup.findAll("img")
+        for l in links:
+            if "media.tumblr.com/tumblr_" in l.attrs['src']:
+                url = l.attrs['src']
+                #img = div.find("img")
+                #url = img.attrs["src"]
+                try:
+                    photo = requests.get(url)
+                    image = Image.open(StringIO(photo.content))
+                    image.verify()
+                    Image.open(StringIO(photo.content)).save(self.path + "." + image.format.lower())
+                    self.submission.unsave()
+                except Exception, e:
+                    errors.append(self.submission.title)
+                    print e
+
+    def flickr_link(self):
+        photo = requests.get(self.submission.url)
+        fkr = Soup(photo.content)
+        div = fkr.find("div", {"class": "photo-div"})
+        photo = div.find("img")
+        url = photo.attrs['src']
+        try:
+            photo = requests.get(url)
+            image = Image.open(StringIO(photo.content))
+            image.verify()
+            Image.open(StringIO(photo.content)).save(self.path + "." + image.format.lower())
+            self.submission.unsave()
+        except Exception, e:
+            errors.append(self.submission.title)
+            print e
+
+    def picsarus_link(self):
+        try:
+            photo = requests.get(self.submission.url + ".jpg")
+            image = Image.open(StringIO(photo.content))
+            image.verify()
+            Image.open(StringIO(photo.content)).save(self.path + "." + image.format.lower())
+            self.submission.unsave()
+        except Exception, e:
+            errors.append(self.submission.title)
+            print e
+
+praw = praw.Reddit("aesptux\'s saved images downloader")
 
 print "Logging in..."
 # create session
-client = requests.session()
-r = client.post(login_url, data=login_payload)
+praw.login(username=username, password=password)
 print "Logged in."
-
-# convert to python dict
-login_response = json.loads(r.text)
-# grab modhash
-client.modhash = login_response['json']['data']['modhash']
-# get all saved links
 print "Getting data..."
-s = requests.get(saved_url)
-s = s.content
-s = json.loads(s)
+# this returns a generator
+saved_links = praw.get_saved_links(limit=None)
 
 # check if dir exists
 if not os.path.exists(save_dir):
@@ -69,156 +177,26 @@ if not os.path.exists(save_dir):
 if not os.path.exists(os.path.join(save_dir, 'albums')):
     os.mkdir(album_path)
 
-# to delete
-names = []
-# to notify errors
-errors = []
+for link in saved_links:
+    # create object per submission. Trusting garbage collector!
+    d = Downloader(link)
 
-while s['data']['children']:
-    for x in s['data']['children']:
-        print "Downloading ---> %s" % (x['data']['title'])
-        try:
-            path = os.path.join(save_dir, str(x['data']['created']) + x['data']['title'][0:15])
-            photo = requests.get(x['data']['url'])
-            # direct link to image
-            if os.path.splitext(photo.url)[1] in ['.jpg', '.gif', '.png', '.jpeg']:
-                try:
-                    i = open(path + os.path.splitext(photo.url)[1], 'w')
-                    i.write(photo.content)
-                    i.close()
-                    if check_size(path + os.path.splitext(photo.url)[1]):
-                        names.append(x['data']['name'])
-                    else:
-                        errors.append(x['data']['title'])
-                except Exception, e:
-                    print e
-            else:
-            # link to imgur
-                if "imgur" in x['data']['domain']:
-                    # its album
-                    if "/a/" in x['data']['url']:
-                        print "It is an imgur album"
-                        download_url = 'http://s.imgur.com/a/%s/zip' % (os.path.split(x['data']['url'])[1])
-                        filezip = requests.get(download_url)
-                        path = os.path.join(album_path, x['data']['title'][0:50])
-                        # extract zip
-                        if not os.path.exists(path):
-                            os.mkdir(path)
-                        try:
-                            i = open(path + '.zip', 'w')
-                            i.write(filezip.content)
-                            i.close()
-                            zipfile = ZipFile(path + '.zip')
-                            zipfile.extractall(path)
-                            names.append(x['data']['name'])
-                        except Exception, e:  # big album
-                            os.remove(path + '.zip')
-                            print "Album is too big, downloading images..."
-                            # this is the best layout
-                            idimage = os.path.split(x['data']['url'])[1]
-                            if '#' in idimage:
-                                idimage = idimage[0:idimage.index("#")]
-                            url = "http://imgur.com/a/%s/layout/blog" % (idimage)
-                            web = requests.get(url)
-                            soup = Soup(web.content)
-                            container = soup.find("div", {"id": "image-container"})
-                            imgs = container.findAll("a", {"class": "zoom"})
-                            counter = 0
-                            for img in imgs:
-                                print counter
-                                p = img.attrs['href']
-                                photo = requests.get(p)
-                                ext = detect_ext(photo.content[0:10])
-                                i = open(path + "/" + str(counter) + ext, 'w')
-                                i.write(photo.content)
-                                i.close()
-                                counter += 1
-
-                            names.append(x['data']['name'])
-                    # imgur image
-                    else:
-                        # just a hack. i dont know if this will be a .jpg, but in order to download an
-                        # image data, I have to write an extension
-                        new_url = "http://i.imgur.com/%s.jpg" % (os.path.split(x['data']['url'])[1])
-                        photo = requests.get(new_url)
-                        ext = detect_ext(photo.content[0:10])
-                        i = open(path + ext, 'w')
-                        i.write(photo.content)
-                        i.close()
-                        if check_size(path + ext):
-                            names.append(x['data']['name'])
-                        else:
-                            errors.append(x['data']['title'])
-                elif "tumblr" in x['data']['domain']:
-                    soup = Soup(photo.content)
-                    #div = soup.find("div", {'class': 'post'})
-                    #if not div:
-                    #    div = soup.find("li", {'class': 'post'})
-                    links = soup.findAll("img")
-                    for l in links:
-                        if l.attrs['src']:
-                            url = l.attrs['src']
-                    #img = div.find("img")
-                    #url = img.attrs["src"]
-                    photo = requests.get(url)
-                    ext = detect_ext(photo.content[0:10])
-                    print path
-                    print ext
-                    i = open(path + ext, 'w')
-                    i.write(photo.content)
-                    i.close()
-                    if check_size(path + ext):
-                        names.append(x['data']['name'])
-                    else:
-                        errors.append(x['data']['title'])
-                elif "flickr" in x['data']['domain']:
-                    fkr = Soup(photo.content)
-                    div = fkr.find("div", {"class": "photo-div"})
-                    photo = div.find("img")
-                    url = photo.attrs['src']
-                    photo = requests.get(url)
-                    ext = detect_ext(photo.content[0:10])
-                    i = open(path + ext, 'w')
-                    i.write(photo.content)
-                    i.close()
-                    if check_size(path + ext):
-                        names.append(x['data']['name'])
-                    else:
-                        errors.append(x['data']['title'])
-                else:
-                    print "%s Domain not supported" % (x['data']['domain'])
-
-        except Exception, e:
-            print "Could not get photo"
-
-    # read data
-    if s['data']['after']:
-        print "Loading next page."
-        s = requests.get(saved_url + '&after=' + s['data']['after'])
-        s = s.content
-        s = json.loads(s)
+    if is_image_link(link):
+        d.direct_link()
     else:
-        print "No more pages."
-        s['data']['children'] = None
-
-unsave_payload = {'id': '', 'uh': client.modhash}
-
-######### unsaving block ########
-for name in names:
-    unsave_payload['id'] = name
-    print "Unsaving %s" % (unsave_payload['id'])
-    # reddit's api restriction. One request per two seconds
-    time.sleep(2)
-    u = client.post(unsave_url, data=unsave_payload, headers=headers)
-################################
-
-for e in errors:
-    print "There was an error with -> %s" % (e)
-
-# check if there are remaining items, to warn the user
-s = requests.get(saved_url)
-s = s.content
-s = json.loads(s)
-
-if s['data']['children']:
-    print "There are still items in your saved links"
+        # not direct, read domain
+        if 'imgur' in link.domain:
+            # check if album
+            if '/a/' in link.url:
+                d.imgur_album()
+            else:
+                d.imgur_link()
+        elif 'tumblr' in link.domain:
+            d.tumblr_link()
+        elif 'flickr' in link.domain:
+            d.flickr_link()
+        elif 'picsarus' in link.domain:
+            d.picsarus_link()
+        else:
+            print "%s ->> Domain not supported" % (link.domain)
+print "Done."
